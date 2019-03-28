ff705a788195e137c890b8dff15a141bc6524dbf
sfernandez <sfernandez@tuenti.com>
2019-03-28 21:48:17 +0100

    unused file

--- src/jira.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchJiraTicket = (ticketId: string, authToken: string) =>
-    axios.get(`https://jira.tuenti.io/jira/rest/api/latest/issue/${ticketId}`, {
-        headers: {Authorization: `Basic ${authToken}`},
-    });
