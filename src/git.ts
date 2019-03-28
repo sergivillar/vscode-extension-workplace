0ca51df062a5af029fbc29215527bea48dd54b0e
sfernandez <sfernandez@tuenti.com>
2019-03-28 10:55:31 +0100

    adding more git commits

--- src/git.ts
+++ src/git.ts
@@ -3,8 +3,14 @@ import {execSync} from 'child_process';
 
 const GET_CURRENT_BRANCH = 'git rev-parse --abbrev-ref HEAD';
 const CREATE_NEW_BRANCH = 'git checkout -b';
+const CHECKOUT_TO_BRANCH = 'git checkout';
+const CHECK_WORKING_TREE_STATUS = 'git status --porcelain';
 
-type CommandOptions = typeof GET_CURRENT_BRANCH | typeof CREATE_NEW_BRANCH;
+type CommandOptions =
+    | typeof GET_CURRENT_BRANCH
+    | typeof CREATE_NEW_BRANCH
+    | typeof CHECKOUT_TO_BRANCH
+    | typeof CHECK_WORKING_TREE_STATUS;
 
 function executeGitCommand(command: CommandOptions, ...args: string[]) {
     if (!vscode.workspace.workspaceFolders) {
@@ -27,6 +33,11 @@ function executeGitCommand(command: CommandOptions, ...args: string[]) {
 const gitCommands = {
     getCurrentBranch: () => executeGitCommand(GET_CURRENT_BRANCH),
     createBranch: (name: string) => executeGitCommand(CREATE_NEW_BRANCH, name),
+    checkoutToBranch: (name: string) => executeGitCommand(CHECKOUT_TO_BRANCH, name),
+    isWorkingTreeClean: () => {
+        const result = executeGitCommand(CHECK_WORKING_TREE_STATUS);
+        return !result;
+    },
 };
 
 export default gitCommands;
